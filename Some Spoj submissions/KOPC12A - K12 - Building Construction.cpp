87f769a162f4decb75114800cf8add3b21aa5a7e
Shreyas-SShetty <54276747+Shreyas-SShetty@users.noreply.github.com>
2021-07-09 14:07:52 +0530

    Create KOPC12A - K12 - Building Construction.cpp

---
+++ Some Spoj submissions/KOPC12A - K12 - Building Construction.cpp
@@ -0,0 +1,104 @@
+/*
+Link to question - https://www.spoj.com/problems/KOPC12A/
+
+Answer lies in between minimum and maximum height
+For some optimum height it will be least
+On either side, it increases
+We can find minima using ternary search
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("-Ofast")
+
+//#define      ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define        fast               ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define        ff                 first
+#define        ss                 second
+#define        show(b)            for(auto const&x: b) cout<<x<<" "; cout << "\n";
+#define        endl               "\n"
+#define        vt                 vector
+#define        chk                cout << "here\n";
+
+#define        trace1(x)                cerr<<#x<<": "<<x<<endl
+#define        trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define        trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define        trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define        trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define        trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+typedef       long long int ll;
+typedef       long double LD;
+typedef       pair<int,int> pii;
+typedef       pair<long long int,long long int> pll;
+const int N=1e5+5;
+const int mod=1e9+7;
+//mt19937 rnd(time(NULL));
+
+/*------Global--------*/
+vt<ll> a, cost;
+/*-----Functions------*/
+
+ll modexp(ll a,ll b,ll mod) {
+    ll x=1;
+    while(b)
+    {
+        if(b&1) x = ((x%mod) * (a%mod))%mod;
+        a = ((a%mod) * (a%mod))%mod;
+        b = b / 2;
+    }
+    return x;
+}
+
+void rf() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+}
+
+ll totalCost(int finalHeight, int n) {
+    ll ans = 0;
+    for(int i = 0; i < n; ++i)
+        ans += (cost[i] * abs(finalHeight - a[i]));
+    return ans;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    a.resize(n);
+    cost.resize(n);
+    for(int i = 0; i < n; ++i)
+        cin >> a[i];
+    for(int i = 0; i < n; ++i)
+        cin >> cost[i];
+    ll l = *min_element(a.begin(), a.end());
+    ll h = *max_element(a.begin(), a.end());
+    while((l + 5) <= h) {
+        ll m1 = l + (h - l) / 3;
+        ll m2 = h - (h - l) / 3;
+        if(totalCost(m1, n) < totalCost(m2, n))
+            h = m2;
+        else
+            l = m1;
+    }
+    ll ans = totalCost(l, n);
+    for(ll i = l + 1; i <= h; ++i)
+        ans = min(ans, totalCost(i, n));
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    fast;
+    //cin.exceptions(cin.failbit);
+    rf();
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
